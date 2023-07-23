fd3e56d0131e4066ab2ad4b42072439c40b3b6d9
Siyoung Park <drq5@daum.net>
2023-07-23 20:46:01 +0900

    [feat] FollowService 기능 추가

--- src/main/java/com/example/taskqueue/follow/entity/Follow.java
+++ src/main/java/com/example/taskqueue/follow/entity/Follow.java
@@ -19,7 +19,7 @@ public class Follow {
     private Long id;
 
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
