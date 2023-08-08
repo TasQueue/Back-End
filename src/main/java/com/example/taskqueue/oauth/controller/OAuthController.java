a0123a3c156c9b25dd2ebae62336616d67ae67db
sheisalice606 <1010qazs2@gmail.com>
2023-08-08 13:38:51 +0900

    [feat] swagger 연결작업

--- src/main/java/com/example/taskqueue/oauth/controller/OAuthController.java
+++ src/main/java/com/example/taskqueue/oauth/controller/OAuthController.java
@@ -2,20 +2,24 @@ package com.example.taskqueue.oauth.controller;
 
 
 import com.example.taskqueue.common.annotation.CurrentUser;
+import com.example.taskqueue.exceptionhandler.ErrorResponse;
 import com.example.taskqueue.oauth.service.CustomOAuth2UserService;
 import com.example.taskqueue.oauth.service.LogoutService;
+import com.example.taskqueue.user.controller.dto.response.GetUserDto;
 import com.example.taskqueue.user.entity.User;
 import com.example.taskqueue.user.repository.UserRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@Api(tags = "OAuth API")
+@RestController
 @RequiredArgsConstructor
 public class OAuthController {
     private final LogoutService logoutService;
@@ -29,8 +33,16 @@ public class OAuthController {
         logoutService.logoutUser(accessToken,createdUser);
         return "redirect:/";
     }
+
+
+    @ApiOperation(
+            value = "자신의 소셜로그인 정보 확인하기.",
+            notes = "자신의 소셜로그인 정보를 확인한다."
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK", response = String.class),
+    })
     @GetMapping("/user-info")
-    @ResponseBody
     public String getUserInfo(@CurrentUser User user) {
         if (user != null) {
             return "Hello, " + user.toString();
