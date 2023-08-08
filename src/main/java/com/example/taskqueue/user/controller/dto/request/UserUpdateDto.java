0e7a31c6031de7f027dc4ae4fc2279ce845738ed
sheisalice606 <1010qazs2@gmail.com>
2023-08-08 13:09:16 +0900

    [feat] swagger 연결작업

--- src/main/java/com/example/taskqueue/user/controller/dto/request/UserUpdateDto.java
+++ src/main/java/com/example/taskqueue/user/controller/dto/request/UserUpdateDto.java
@@ -1,6 +1,7 @@
 package com.example.taskqueue.user.controller.dto.request;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,14 +11,14 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 public class UserUpdateDto {
 
-    @Schema(description = "변경할 유저 이름(변경 안하더라도 기존 이름 필요)", example = "홍길동")
+    @ApiModelProperty(value = "변경할 유저 이름(변경 안하더라도 기존 이름 필요)", example = "홍길동")
     @NotBlank(message = "유저 이름은 필수 값입니다.")
     private String name;
 
-    @Schema(description = "변경할 한 줄 소개", example = "안녕하세요. 반갑습니다.")
+    @ApiModelProperty(value = "변경할 한 줄 소개", example = "안녕하세요. 반갑습니다.")
     private String intro;
 
-    @Schema(description = "잔디 색깔", example = "ABC123")
+    @ApiModelProperty(value = "잔디 색깔", example = "ABC123")
     @NotBlank(message = "잔디 색깔은 필수 값입니다.")
     private String color;
 
