5ffdba6b7f2bf8e4378756ed60a4c33487bebfc2
JEONG KI MIN <97235034+nimikgnoej@users.noreply.github.com>
2023-08-26 19:15:50 +0900

    [fix] Redis 설정 수정

--- src/main/java/com/example/taskqueue/config/RedisConfig.java
+++ src/main/java/com/example/taskqueue/config/RedisConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -19,7 +20,12 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+
+        config.setPassword(redisProperties.getPassword());
+
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
